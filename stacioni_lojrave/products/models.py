da7e124292032c3a3aceaa0a1f504be95ecf20fc
DionGrajqevci <dngrajqevci@gmail.com>
2023-06-14 14:03:07 +0100

    Added description for products

--- stacioni_lojrave/products/models.py
+++ stacioni_lojrave/products/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
+    description = models.CharField(max_length=5000)
     type = models.CharField(max_length=13, choices=TYPE_CHOICES)
 
 
